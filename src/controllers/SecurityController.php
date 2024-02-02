c875696ba60ba922b233f0f25f804844801d7c76
przemekdan1 <przemekdan1@gmail.com>
2024-02-02 16:38:58 +0100

    user fetched from database, user repository, evnironment for php to connect with PDO, lab08

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,11 +2,14 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 class SecurityController extends AppController
 {
     public function login()
     {
-        $user = new \models\User('a@a.pl','a','Jan','Ab');
+        $userRepository = new UserRepository();
+
+
 
         if(!$this->isPost())
         {
@@ -16,6 +19,12 @@ class SecurityController extends AppController
         $email = $_POST['email'];
         $password = $_POST['password'];
 
+
+        $user = $userRepository->getUser($email);
+        if(!$user)
+        {
+            return $this->render('login',['messages'=>['User not exist']]);
+        }
         if($user->getEmail() !== $email)
         {
             return $this->render('login',['messages'=>['User with this email not exist']]);
