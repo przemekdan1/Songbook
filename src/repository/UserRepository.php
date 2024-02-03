a5f538f269122856bfe3ccf72263b4461361ef68
przemekdan1 <przemekdan1@gmail.com>
2024-02-03 16:46:50 +0100

    add proper path for repair returning User type

--- src/repository/UserRepository.php
+++ src/repository/UserRepository.php
@@ -4,17 +4,17 @@ require_once 'Repository.php';
 require_once __DIR__.'/../models/User.php';
 class UserRepository extends Repository
 {
-    public function getUser(string $email): ?User
+    public function getUser(string $email): ?models\User
     {
         $statement = $this->database->connect()->prepare('
-        SELECT * FROM public."user" WHERE "email"=:email');
+        SELECT * FROM public.users WHERE email = :email');
 
         $statement->bindParam(':email', $email, PDO::PARAM_STR);
         $statement->execute();
 
         $user = $statement->fetch(PDO::FETCH_ASSOC);
 
-        if($user == false)
+        if(!$user)
         {
             return null;
         }
